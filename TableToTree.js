c08df47df49e47f428d677523f20053135303cde
Ajay Kumar R <ajaykumarr3024@gmail.com>
2020-12-10 01:05:43 +0530

    Table to hierarchical tree strcuture using javascript

---
+++ TableToTree.js
@@ -0,0 +1,213 @@
+const data =  [
+    {
+        "id": 0.0,
+        "EMPLOYEE_ID": "42ca4e80-8152-485f-8b50-398f365e8731",
+        "DESIGNATION": "Co-founder",
+        "DEPARTMENT": "Co-Founder",
+        "NAME": "Sachin Tendulkar",
+        "MANAGEREMPLOYEE_ID": "40e5b5ac-e921-408a-9bf3-8c4e3c0d8fc8"
+    },
+    {
+        "id": 1.0,
+        "EMPLOYEE_ID": "40e5b5ac-e921-408a-9bf3-8c4e3c0d8fc8",
+        "DESIGNATION": "Co-Founder",
+        "DEPARTMENT": "Co-Founder",
+        "NAME": "Saurav Ganguly",
+        "MANAGEREMPLOYEE_ID": ""
+    },
+    {
+        "id": 2.0,
+        "EMPLOYEE_ID": "TENANTPL/003612",
+        "DESIGNATION": "CTO",
+        "DEPARTMENT": "TECH",
+        "NAME": "Mohammad Azharuddin",
+        "MANAGEREMPLOYEE_ID": "40e5b5ac-e921-408a-9bf3-8c4e3c0d8fc8"
+    },
+    {
+        "id": 3.0,
+        "EMPLOYEE_ID": "0c2f515e-8b09-4da7-a4ca-94b51bf4a954",
+        "DESIGNATION": "Credit Head",
+        "DEPARTMENT": "Credit",
+        "NAME": "Shruti Hassan",
+        "MANAGEREMPLOYEE_ID": "42ca4e80-8152-485f-8b50-398f365e8731"
+    },
+    {
+        "id": 4.0,
+        "EMPLOYEE_ID": "TENANTPL/005896",
+        "DESIGNATION": "SDE3",
+        "DEPARTMENT": "TECH",
+        "NAME": "Jaspreet Bumrah",
+        "MANAGEREMPLOYEE_ID": "TENANTPL/003612"
+    },
+    {
+        "id": 5.0,
+        "EMPLOYEE_ID": "TENANTPL/006867",
+        "DESIGNATION": "Business Development",
+        "DEPARTMENT": "Sales",
+        "NAME": "Sunil Manohar Gavaskar",
+        "MANAGEREMPLOYEE_ID": "40e5b5ac-e921-408a-9bf3-8c4e3c0d8fc8"
+    },
+    {
+        "id": 6.0,
+        "EMPLOYEE_ID": "TENANTPL/007547",
+        "DESIGNATION": "Business Development",
+        "DEPARTMENT": "Sales",
+        "NAME": "Bikash Agrawal",
+        "MANAGEREMPLOYEE_ID": "40e5b5ac-e921-408a-9bf3-8c4e3c0d8fc8"
+    },
+    {
+        "id": 7.0,
+        "EMPLOYEE_ID": "TENANTPL/008682",
+        "DESIGNATION": "Operation Executive",
+        "DEPARTMENT": "Operation",
+        "NAME": "Mohammad Shahabaaz",
+        "MANAGEREMPLOYEE_ID": "40e5b5ac-e921-408a-9bf3-8c4e3c0d8fc8"
+    },
+    {
+        "id": 8.0,
+        "EMPLOYEE_ID": "TENANTPL/009152",
+        "DESIGNATION": "Operation Executive",
+        "DEPARTMENT": "Operation",
+        "NAME": "Ashish Nehra",
+        "MANAGEREMPLOYEE_ID": "TENANT234548ffdbcc1cf1403685c51d48f6baf8bf"
+    },
+    {
+        "id": 9.0,
+        "EMPLOYEE_ID": "TENANTPL/012431",
+        "DESIGNATION": "Co-Founder",
+        "DEPARTMENT": "Operation",
+        "NAME": "Amitabh Harivansh Rai Bachan",
+        "MANAGEREMPLOYEE_ID": "TENANTPL/006867"
+    },
+    {
+        "id": 10.0,
+        "EMPLOYEE_ID": "TENANTPL/013360",
+        "DESIGNATION": "Business Development",
+        "DEPARTMENT": "Sales",
+        "NAME": "Shah Rukh Rukh Khan",
+        "MANAGEREMPLOYEE_ID": "40e5b5ac-e921-408a-9bf3-8c4e3c0d8fc8"
+    },
+    {
+        "id": 11.0,
+        "EMPLOYEE_ID": "TENANTPL/014432",
+        "DESIGNATION": "Disbursal Executive",
+        "DEPARTMENT": "Operation",
+        "NAME": "Virat  Kohli",
+        "MANAGEREMPLOYEE_ID": "40e5b5ac-e921-408a-9bf3-8c4e3c0d8fc8"
+    },
+    {
+        "id": 12.0,
+        "EMPLOYEE_ID": "TENANTo/124cf4a894145c49318d819067f1ab2b11",
+        "DESIGNATION": "SDE2",
+        "DEPARTMENT": "Tech",
+        "NAME": "Rohit K Sharma",
+        "MANAGEREMPLOYEE_ID": "TENANTPL/012431"
+    },
+    {
+        "id": 13.0,
+        "EMPLOYEE_ID": "TENANT122388163c8704264e02a0b6a70e4fb6d4f6",
+        "DESIGNATION": "SDE",
+        "DEPARTMENT": "TECH",
+        "NAME": "Ashish Karan Agarwal",
+        "MANAGEREMPLOYEE_ID": "TENANTo/124cf4a894145c49318d819067f1ab2b11"
+    },
+    {
+        "id": 14.0,
+        "EMPLOYEE_ID": "TENANT12226d3fc48078074975aa44570123a17a6b",
+        "DESIGNATION": "SDE",
+        "DEPARTMENT": "TECH",
+        "NAME": "Manish Sharad Thakur",
+        "MANAGEREMPLOYEE_ID": "TENANTo/124cf4a894145c49318d819067f1ab2b11"
+    },
+    {
+        "id": 15.0,
+        "EMPLOYEE_ID": "TENANTwqwqa318a927cea74db39107ef9e074bb64d",
+        "DESIGNATION": "SDE",
+        "DEPARTMENT": "TECH",
+        "NAME": "Rohan Gavaskar",
+        "MANAGEREMPLOYEE_ID": "TENANTo/124cf4a894145c49318d819067f1ab2b11"
+    },
+    {
+        "id": 16.0,
+        "EMPLOYEE_ID": "TENANT2211180cf6739a8245b1ab32b2f3dac17662",
+        "DESIGNATION": "CMO",
+        "DEPARTMENT": "Marketing",
+        "NAME": "Manish S Sharma",
+        "MANAGEREMPLOYEE_ID": "42ca4e80-8152-485f-8b50-398f365e8731"
+    },
+    {
+        "id": 17.0,
+        "EMPLOYEE_ID": "TENANT123410c882b2fe93484a9ac69d92f1e7c0b9",
+        "DESIGNATION": "SDE",
+        "DEPARTMENT": "TECH",
+        "NAME": "Manisha Koirala",
+        "MANAGEREMPLOYEE_ID": "TENANTo/124cf4a894145c49318d819067f1ab2b11"
+    },
+    {
+        "id": 18.0,
+        "EMPLOYEE_ID": "59b83532b32e4bada65245fbffe77505",
+        "DESIGNATION": "SDE",
+        "DEPARTMENT": "TECH",
+        "NAME": "Anand Tandon",
+        "MANAGEREMPLOYEE_ID": "TENANTo/124cf4a894145c49318d819067f1ab2b11"
+    },
+    {
+        "id": 19.0,
+        "EMPLOYEE_ID": "efd782d0f2754363b12a17fed68639f4",
+        "DESIGNATION": "SDE",
+        "DEPARTMENT": "TECH",
+        "NAME": "Runa Laila",
+        "MANAGEREMPLOYEE_ID": "TENANTo/124cf4a894145c49318d819067f1ab2b11"
+    }
+]
+
+// algorithm for making the tabular data into organized tree structure
+var newData = { name :"root", children : [] },
+    levels = ["NAME", "EMPLOYEE_ID", "MANAGEREMPLOYEE_ID"];
+
+// For each data row, loop through the expected levels traversing the output tree
+data.forEach(function(d){
+    // Keep this as a reference to the current level
+    var depthCursor = newData.children;
+    // Go down one level at a time
+    levels.forEach(function( property, depth ){
+
+        // Look to see if a branch has already been created
+        var index;
+        depthCursor.forEach(function(child,i){
+            if ( d[property] == child.name ) index = i;
+        });
+        // Add a branch if it isn't there
+        if ( isNaN(index) ) {
+            depthCursor.push({ name : d[property], children : []});
+            index = depthCursor.length - 1;
+        }
+        // Now reference the new child array as we go deeper into the tree
+        depthCursor = depthCursor[index].children;
+        // This is a leaf, so add the last element to the specified branch
+        if ( depth === levels.length - 1 ) depthCursor.push({ designation : d.DESIGNATION, department : d.DEPARTMENT });
+    });
+});
+
+console.log(newData)
+
+// function to render hierarchical view
+function getNestedChildren(arr, parent) {
+    var out = []
+    for (var i in arr) {
+      if (arr[i].MANAGEREMPLOYEE_ID == parent) {
+        var children = getNestedChildren(arr, arr[i].EMPLOYEE_ID)
+  
+        if (children.length) {
+          arr[i].children = children
+        }
+        out.push(arr[i])
+      }
+    }
+    return out
+  }
+
+  
+var nested = getNestedChildren(data, '40e5b5ac-e921-408a-9bf3-8c4e3c0d8fc8')
+
+document.write('<pre>' + JSON.stringify(nested, 0, 4) + '</pre>');
